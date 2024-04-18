b4f7c3732ccd662010a0cc7242cd1c54ef981ab2
Raghava Jagannatham <raghavajagannatham9@gmail.com>
2024-04-18 16:02:19 +0530

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+/** @format */
+
+import PageTitle from "@/components/PageTitle";
+
+
+
+export default function Home() {
+  return (
+    <div className="flex flex-col gap-5  w-full">
+      <PageTitle title="Dashboard" />
+      <h1>this is dashboard</h1>
+    </div>
+  );
+}
