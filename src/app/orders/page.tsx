9e2d107241b35bc87bfd6742313d55e27652bfa1
Raghava Jagannatham <raghavajagannatham9@gmail.com>
2024-04-18 16:03:21 +0530

    Update page.tsx

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,1 +1,30 @@
-Enter file contents here
+/**
+ * eslint-disable @next/next/no-img-element
+ *
+ * @format
+ */
+
+/**
+ * eslint-disable @next/next/no-img-element
+ *
+ * @format
+ */
+
+/** @format */
+"use client";
+
+import React from "react";
+import PageTitle from "@/components/PageTitle";
+
+type Props = {};
+
+
+
+export default function OrdersPage({}: Props) {
+  return (
+    <div className="flex flex-col gap-5  w-full">
+      <PageTitle title="Orders" />
+      <h1>This is the Order page</h1>
+    </div>
+  );
+}
