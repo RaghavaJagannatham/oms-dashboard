2eaf1cbe528e44f8a696fcbcde3259079dcc4cec
Raghava Jagannatham <raghavajagannatham9@gmail.com>
2024-04-18 16:03:48 +0530

    Update page.tsx

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,1 +1,32 @@
+/**
+ * eslint-disable @next/next/no-img-element
+ *
+ * @format
+ */
+
+/**
+ * eslint-disable @next/next/no-img-element
+ *
+ * @format
+ */
+
+/** @format */
+"use client";
+
+
+import React from "react";
+import PageTitle from "@/components/PageTitle";
+
+
+type Props = {};
+
+
+export default function SettingsPage({}: Props) {
+  return (
+    <div className="flex flex-col gap-5  w-full">
+      <PageTitle title="Settings" />
+      <h1>This is the Settings</h1>
+    </div>
+  );
+}
 
